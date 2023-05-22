327c5f8c4f1219002d7c59a6c60af786101e3e35
Samuel Norris <samuel.norris@cultureamp.com>
2023-05-22 18:01:49 +1000

    :package: Install react-router-dom, and render index route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello React!</div>,
+    },
+]);
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
-root.render(<h1>Hello React!</h1>);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
